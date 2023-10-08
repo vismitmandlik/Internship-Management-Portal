d5e82e6e463690cc880ac71e7f74bbb87b7fe594
vismitmandlik <108935443+vismitmandlik@users.noreply.github.com>
2023-10-08 19:17:26 +0530

    Add files via upload

--- Internship Management Portal/scripts/script.js
+++ Internship Management Portal/scripts/script.js
@@ -1,3 +1,11 @@
 function Location() {
     window.location.href = "/pages/dashboard.html";
 }
+
+function Conformation() {
+    window.location.href = "/pages/conformation_page.html";
+}
+
+function Approval() {
+    window.location.href = "/pages/approved.html";
+}
